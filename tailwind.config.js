2338f3842e1b2af7ab94a78d94312c16be2fa703
mirza-mohibul-hasan <mirzamohibul618@gmail.com>
2023-07-30 22:14:00 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        'custom-top-border-color': 'rgba(255, 255, 255, 0.20)',
+      },
+    },
+  },
+  plugins: [],
+}
+
